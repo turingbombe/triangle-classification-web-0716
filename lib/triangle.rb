157eb5e9c2135d6b9d1f20f2cac9c0ad4da53580
Charlie Cook <turingbombe@gmail.com>
2016-08-01 18:38:13 -0400

    Feeling very scalene after this one

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,49 @@
+require 'pry'
+
 class Triangle
   # write code here
+  attr_accessor :a, :b, :c, :test_triangle, :triangle_array
+  def initialize(a,b,c)
+    @a = a
+    @b = b
+    @c = c
+    @triangle_array = [a,b,c]
+  end
+
+  def kind
+    # binding.pry
+    test = test_triangle
+    if test == true
+      if ((a == b) && (a == c) && (b == c))
+        return :equilateral
+      elsif ((a == b) || (a == c) || (b == c))
+        return :isosceles
+      elsif a != b && b != c && a!=c 
+        return :scalene
+      end
+    else
+      begin
+        raise TriangleError
+      end 
+    end
+
+  end
+
+  def test_triangle
+    triangle_array.sort!
+    if triangle_array[0] + triangle_array[1] <= triangle_array[2]
+      return false
+    elsif a < 0 || b < 0 || c < 0
+      return false
+    elsif triangle_array.include?(0)
+      return false
+    else
+      return true
+    end
+  end
+end
+
+
+class TriangleError < StandardError
+
 end
